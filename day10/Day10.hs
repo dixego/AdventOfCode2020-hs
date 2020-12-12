b42edbd4bf1d7d7bd56da7114483a770b4eb99d2
dixego <dixego@gmail.com>
2020-12-12 13:46:07 -0600

    Day 10: Typo

--- day10/Day10.hs
+++ day10/Day10.hs
@@ -7,13 +7,14 @@ pairs = zip <*> tail
 completeSortedAdapters :: [Int] -> [Int]
 completeSortedAdapters ad = sort $ (0:ad) ++ [maximum ad + 3]
 
-nbonnaci n = go
+nbonacci :: Int -> [Int]
+nbonacci n = go
   where go = take n (repeat 0) ++ [1] ++ z n
         z 1 = zipWith (+) go (drop 1 go)
         z x = zipWith (+) (z (x-1)) (drop x go)
 
 tri :: Int -> Int
-tri n = nbonnaci 2 !! (n+2)
+tri n = nbonacci 2 !! (n+2)
 
 part1 :: [String] -> Int
 part1 input = ones * threes
