0d6f7cabc179e2f9c441852250463b1bb8d1e479
dixego <dixego@gmail.com>
2020-12-24 15:57:47 -0600

    Day 24

---
+++ day24/Day24.hs
@@ -0,0 +1,71 @@
+import Text.Parsec
+import Linear.V3
+import Data.Set (Set, (\\))
+import qualified Data.Set as Set
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Either (rights)
+import Data.Foldable (toList)
+
+type Parser = Parsec String ()
+type Coord = V3 Int
+
+data Direction = E | SE | SW | W | NW | NE deriving (Show)
+
+move :: Direction -> Coord -> Coord
+move E  = (+ (V3 1 (-1) 0))
+move W  = (+ (V3 (-1) 1 0))
+move NE = (+ (V3 1 0 (-1)))
+move NW = (+ (V3 0 1 (-1)))
+move SE = (+ (V3 0 (-1) 1))
+move SW = (+ (V3 (-1) 0 1))
+
+parseLine :: Parser [Direction]
+parseLine = many1 parseDirection
+  where
+    parseDirection = choice $ map try [northEast, southEast, east, southWest, northWest, west]
+    east = E <$ string "e"
+    west = W <$ string "w"
+    northEast = NE <$ string "ne"
+    northWest = NW <$ string "nw"
+    southEast = SE <$ string "se"
+    southWest = SW <$ string "sw"
+
+parseInput :: String -> [[Direction]]
+parseInput = rights . map (parse parseLine "") . lines
+
+followDirections :: Coord -> [Direction] -> Coord
+followDirections = foldl (flip move)
+
+freqs :: (Foldable f, Ord a) => f a -> Map a Int
+freqs = Map.fromListWith (+) . flip zip (repeat 1) . toList
+
+neighbors :: Coord -> Set Coord
+neighbors c = Set.fromList $ map (c+) ns
+  where ns = [V3 1 (-1) 0, V3 (-1) 1 0,
+              V3 1 0 (-1), V3 (-1) 0 1,
+              V3 0 1 (-1), V3 0 (-1) 1]
+
+neighborCount :: Set Coord -> Map Coord Int
+neighborCount tiles = Map.unionsWith (+) ns
+  where ns = [Map.fromSet (const 1) (neighbors t) | t <- Set.toList tiles]
+
+step :: Set Coord -> Set Coord
+step bs = old <> new 
+  where ns = neighborCount bs
+        old = Map.keysSet.Map.filterWithKey (\k v -> Set.member k bs && (v == 1||v == 2)) $ ns
+        new = Map.keysSet.Map.filterWithKey (\k v -> Set.notMember k bs && v==2) $ ns
+
+part0 :: String -> Map Coord Int
+part0 = freqs . map (followDirections $ pure 0) . parseInput
+
+part1 :: String -> Int
+part1 = Map.size . Map.filter odd . part0
+
+part2 :: String -> Int
+part2 = Set.size . (!!100) . iterate step . Map.keysSet . Map.filter odd . part0
+
+main = do
+  input <- readFile "input.txt"
+  print $ part1 input
+  print $ part2 input
---
+++ day24/README.md
@@ -0,0 +1,6 @@
+It's nice when you know you've solved this problem before so there's no need to worry.
+
+Anyway, yeah, this is basically day 17 again but with a slightly different coordinate
+system.
+
+Merry chrismn
